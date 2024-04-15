3f289c6aa95d8fe5f813047d2f679b9be27ac341
Julia Lumi Suzuki <133894726+LumiJulia@users.noreply.github.com>
2024-04-15 12:28:34 -0300

    Add files via upload

---
+++ A15/A15-Cadastro.sql
@@ -0,0 +1,60 @@
+create table pessoas (
+nome varchar(30),
+idade int,
+sexo char(1),
+peso float,
+altura float,
+nacionalidade varchar(20)
+);
+
+use cadastro
+
+alter table pessoas
+add column profissao varchar(10);
+
+desc pessoas
+
+
+alter table pessoas
+add column profissao varchar(10) after nome;
+
+alter table pessoas 
+add codigo int first
+
+alter table pessoas 
+add cpf int
+
+alter table pessoas 
+modify column cpf varchar(11) not null default '';
+
+alter table pessoas 
+change column cpf rg varchar(11) not null default '';
+
+alter table pessoas 
+rename to gentinha;
+
+drop table if exists gentinha;
+
+show tables;
+
+create table if not exists pessoas (
+id_pessoas int not null auto_increment,
+nome varchar (30) not null,
+nascimento date,
+sexo enum ('M', 'F'),
+peso decimal (5,2),
+altura decimal (3,2),
+nacionalidade varchar(20) default 'Brasil',
+primary key (id_pessoas)
+);
+
+create table if not exists cursos (
+id_cursos int not null auto_increment,
+nome varchar(30) not null unique,
+descricao text,
+carga int unsigned,
+ano year default '2024',
+primary key (id_cursos)
+)default charset  = utf8;
+
+show tables
